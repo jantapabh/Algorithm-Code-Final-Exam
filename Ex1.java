f45861e2f606c320a70b9bb4c2508eda343758bb
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-10 22:25:10 +0700

    Add Ex 1

---
+++ Ex1.java
@@ -0,0 +1,10 @@
+public class Ex1{
+    public static void main(String args[]){
+     String str = new String("My .com site is BeginnersBook.com");
+     System.out.print("String after replacing all com with net: " );
+     System.out.println(str.replaceAll("com", "net"));
+         
+     System.out.print("Replacing whole String: " );
+     System.out.println(str.replaceAll("(.*)Beginners(.*)", "Welcome"));
+    }
+ }
