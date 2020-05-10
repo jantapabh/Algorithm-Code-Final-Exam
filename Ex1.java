16ac84d71c0081e9c1aef93951ad76e73802d556
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-10 22:44:51 +0700

    add commit

--- Ex1.java
+++ Ex1.java
@@ -1,12 +1,17 @@
-public class Ex1{
 
+public class Ex1{
     public static void main(String args[]){
-     String str = new String("My .com site is BeginnersBook.com");
-     System.out.print("String after replacing all com with net: " );
-     System.out.println(str.replaceAll("com", "net"));
-         
-     System.out.print("Replacing whole String: " );
-     System.out.println(str.replaceAll("(.*)Beginners(.*)", "Welcome"));
+
+        String Str = new String("Welcome to geeksforgeeks"); 
+        
+        // Using replace to replace characters 
+        System.out.print("After replacing all o with T : " ); 
+        System.out.println(Str.replace('o', 'T')); 
+          
+        // Using replace to replace characters 
+        System.out.print("After replacing all e with D : " ); 
+        System.out.println(Str.replace('e', 'D')); 
+        
     }
-    
+
  }
