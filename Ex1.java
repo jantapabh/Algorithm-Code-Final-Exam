533cffa7a2e5838cbe1c1e29873f1cf7ef36ee5b
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-11 22:37:54 +0700

    add commit

--- Ex1.java
+++ Ex1.java
@@ -1,13 +1,24 @@
 public class Ex1 {
 
-    public static void main(String args[]){
-        
-        // Initialising String 
-        String str = "we are happy";
+    public static void main(String args[]) {
 
+        // Initialising String
+        String str = "we are happy";
+        String str2 = "we%20are%20happy";
+
+        char[] ch = new char[str2.length()];
+
+        System.out.println("Before Add %20 : ");
         System.out.println(str);
-    
-         
+
+        for (int i = 0; i < str2.length(); i++) {
+            ch[i] = str2.charAt(i);
+
+        }
+
+        System.out.println("After Add %20 : ");
+        System.out.println(ch);
+
     }
-    
+
 }
